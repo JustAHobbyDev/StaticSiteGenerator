877930245f95a6b54cf2857fd809f9545194bf1c
Daniel Schmidt <justahobbydev@proton.me>
2024-04-29 02:59:13 -0500

    Implements first naive design for HTMLNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -3,20 +3,29 @@ from typing import Mapping, List, ForwardRef
 
 
 class HTMLNode:
-    def __init__(self, tag: str, content: str, children: List[HTMLNode], props: Mapping[str, str]) -> None:
+    def __init__(self, 
+                 tag: str = "", 
+                 content: str = "", 
+                 children: List[HTMLNode] = [], 
+                 props: Mapping[str, str] = {}) -> None:
         self.tag = tag
         self.content = content
         self.children = children
         self.props = props
 
         
-    def to_html(self):
-        raise NotImplemented
+    def to_html(self) -> str:
+        html = f"<{self.tag}{self.props_to_html()}>{self.content}</{self.tag}>"
+        return html
     
 
-    def props_to_html(self):
-        raise NotImplemented
-    
+    def props_to_html(self) -> str:
+        props_str = ""
+        for prop in self.props:
+            props_str += " "
+            props_str += f"{prop}=\"{self.props[prop]}\""
+        return props_str 
 
-    def __repr__(self):
-        raise NotImplemented
+    def __repr__(self) -> str:
+        r = f"<{__class__.__name__}:\ntag: {self.tag}\ncontent: {self.content}\nchildren: {self.children}\nprops: {self.props}\n>"
+        return r
