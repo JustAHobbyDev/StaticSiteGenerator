80eb66b0108fc45603321deb94b912c5f9343cd6
Daniel Schmidt <justahobbydev@proton.me>
2024-04-30 17:03:16 -0500

    Can split TextNodes by delimiter text types

--- src/main.py
+++ src/main.py
@@ -25,12 +25,64 @@ def text_node_to_html_node(text_node):
     return leaf_node
 
 
-tn = TextNode("Some random text", "italic", "https://boot.dev")
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    final_nodes = []
+    for node in old_nodes:
+        if node.__class__.__name__ == "TextNode":
+            future_textnodes = []
+            delimited = False
+            i, j = 0, 0
+            while j < len(node.text):
+                character = node.text[j]
+                # print(character)
+                if character is delimiter:
+                    content = node.text[i:j]
+                    future_textnodes.append({"content": content, "delimited": delimited})
+                    delimited = not delimited
+                    i = j + 1
+                    j = i + 1
+                else:
+                    if j < len(node.text):
+                        j += 1
+            else:
+                # delimited should be False after iterating through the text
+                if delimited:
+                    raise SyntaxError("Invalid Markdown: Unclosed delimiter found.") 
+                print(node.text[i:j])
+                content = node.text[i:j]
+                future_textnodes.append({"content": content, "delimited": delimited})
 
-print(tn)
+                for future_textnode in future_textnodes:
+                    if not future_textnode["delimited"]:
+                        future_textnode_type = "text"
+                    else:
+                        future_textnode_type = text_type
+                    final_nodes.append(TextNode(future_textnode["content"], future_textnode_type, node.url))
 
-print(text_node_to_html_node(tn))
+        else:
+            final_nodes.append(node)
 
-img_tn = TextNode("A pretty picture", "image", "http://unsplash.com/photos/a-bird-is-standing-on-a-hill-at-sunset-5AiN3pLyJLU")
-img = text_node_to_html_node(img_tn)
-print(img.to_html())
+    return final_nodes
+
+
+# tn = TextNode("Some random text", "italic", "https://boot.dev")
+
+# print(tn)
+
+# print(text_node_to_html_node(tn))
+
+# img_tn = TextNode("A pretty picture", "image", "http://unsplash.com/photos/a-bird-is-standing-on-a-hill-at-sunset-5AiN3pLyJLU")
+# img = text_node_to_html_node(img_tn)
+# print(img.to_html())
+
+node = TextNode("This is text with a `code block` word", "text")
+print(f"type of node: {type(node)}")
+new_nodes = split_nodes_delimiter([node], "`", "code")
+print(new_nodes)
+
+
+bad_node = TextNode("This is text with a `code block word", "text")
+try:
+    new_nodes = split_nodes_delimiter([node, bad_node], "`", "code")
+except Exception as e:
+    print(f"Error: {e}")
