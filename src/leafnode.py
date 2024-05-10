5a22cb4a10cd92151955907d2cfedd8f94c88dad
Daniel Schmidt <justahobbydev@proton.me>
2024-05-10 02:13:44 -0500

    Implement __eq__ for LeafNode

--- src/leafnode.py
+++ src/leafnode.py
@@ -8,6 +8,12 @@ class LeafNode(HTMLNode):
         self.props = props
         super().__init__(tag, content, None, props)
 
+
+    def __eq__(self, other):
+        """Tests equality of two LeafNodes"""
+        s, o = self, other
+        return s.tag == o.tag and s.content == o.content and s.props == o.props
+        
         
     def __repr__(self):
         return super().__repr__(self)
