7425d76416dc1a48c601465ff1c1daee45bc86d3
Daniel Schmidt <justahobbydev@proton.me>
2024-05-17 07:23:42 -0500

    Add inline image and link parsing. Tests RED

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -11,11 +11,13 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             delimited = False
             i, j = 0, 0
             while j < len(node.text):
-                character = node.text[j:j+len(delimiter)]
+                character = node.text[j : j + len(delimiter)]
                 # print(character)
                 if character == delimiter:
                     content = node.text[i:j]
-                    future_textnodes.append({"content": content, "delimited": delimited})
+                    future_textnodes.append(
+                        {"content": content, "delimited": delimited}
+                    )
                     delimited = not delimited
                     i = j + delimiter_jump
                     j = i + delimiter_jump
@@ -25,7 +27,7 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             else:
                 # delimited should be False after iterating through the text
                 if delimited:
-                    raise SyntaxError("Invalid Markdown: Unclosed delimiter found.") 
+                    raise SyntaxError("Invalid Markdown: Unclosed delimiter found.")
                 print(node.text[i:j])
                 content = node.text[i:j]
                 future_textnodes.append({"content": content, "delimited": delimited})
@@ -35,7 +37,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                         future_textnode_type = "text"
                     else:
                         future_textnode_type = text_type
-                    final_nodes.append(TextNode(future_textnode["content"], future_textnode_type, node.url))
+                    final_nodes.append(
+                        TextNode(
+                            future_textnode["content"], future_textnode_type, node.url
+                        )
+                    )
 
         else:
             final_nodes.append(node)
@@ -51,3 +57,64 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     matches = re.findall(r"(?:[^!])\[(.*?)\]\((.*?)\)", text)
     return matches
+
+
+def scan_until_not_text(t):
+    i, j, k = 0, 0, 0
+    open_square, open_round = False, False
+    parsing_text = True
+    while j < len(t):
+        if t[j] == "[":
+            if not parsing_text:
+                break
+            parsing_text = False
+            open_square = True
+            if j - 1 > 0 and t[j - 1] == "!":
+                k = j - 1
+            else:
+                k = j
+        if not parsing_text:
+            if open_square:
+                if t[j] == "]" and t[j + 1] == "(":
+                    open_square = False
+                    open_round = True
+            if open_round and not open_square:
+                if t[j] == ")":
+                    open_square = False
+                    return t[i:k], k
+        j += 1
+    return t, -1
+
+def parse_links_and_images(t):
+    chunks = []
+    i, j, k = 0, 0, 0
+    open_square, open_round = False, False
+    parsing_text = True
+    while j < len(t):
+        if t[j] == "[":
+            if not parsing_text:
+                break
+            parsing_text = False
+            open_square = True
+            if j - 1 > 0 and t[j - 1] == "!":
+                k = j - 1
+            else:
+                k = j
+        if not parsing_text:
+            if open_square:
+                if t[j] == "]" and t[j + 1] == "(":
+                    open_square = False
+                    open_round = True
+            if open_round and not open_square:
+                if t[j] == ")":
+                    open_round = False
+                    parsing_text = True
+                    chunks.append(t[i:k])
+                    chunks.append(t[k:j+1])
+                    t = t[j+1:]
+                    i = 0
+                    j = 0
+        j += 1
+    if len(t) > 0:
+        chunks.append(t)
+    return chunks
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,11 @@
 import unittest
 
-from inline_markdown import extract_markdown_images, split_nodes_delimiter
+from inline_markdown import (
+    extract_markdown_images,
+    parse_links_and_images,
+    scan_until_not_text,
+    split_nodes_delimiter,
+)
 from inline_markdown import extract_markdown_links
 from textnode import TextNode
 
@@ -10,37 +15,139 @@ class TestInlineMarkdown(unittest.TestCase):
         text = "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and ![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)"
         print()
         got = extract_markdown_images(text)
-        want = [("image", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"), ("another", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png")]
+        want = [
+            (
+                "image",
+                "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png",
+            ),
+            (
+                "another",
+                "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png",
+            ),
+        ]
         self.assertEqual(want, got)
 
     def test_extract_markdown_links(self):
         text = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"
         got = extract_markdown_links(text)
-        want = [("link", "https://www.example.com"), ("another", "https://www.example.com/another")]
+        want = [
+            ("link", "https://www.example.com"),
+            ("another", "https://www.example.com/another"),
+        ]
         self.assertEqual(want, got)
-
 
     def test_split_nodes_delimiter_bad_node(self):
         node = TextNode("This is text with a `code block` word", "text")
         bad_node = TextNode("This is text with a `code block word", "text")
-        self.assertRaises(SyntaxError, split_nodes_delimiter, [node, bad_node], "`", "code")
+        self.assertRaises(
+            SyntaxError, split_nodes_delimiter, [node, bad_node], "`", "code"
+        )
 
     def test_bold(self):
         bold = TextNode("This is a *BOLD* statement!", "text")
-        want = [TextNode("This is a ", "text"), TextNode("BOLD", "bold"), TextNode(" statement!", "text")]
-        got = split_nodes_delimiter([bold], '*', "bold")
+        want = [
+            TextNode("This is a ", "text"),
+            TextNode("BOLD", "bold"),
+            TextNode(" statement!", "text"),
+        ]
+        got = split_nodes_delimiter([bold], "*", "bold")
         self.assertEqual(want, got)
-
 
     def test_bold_double(self):
         bold = TextNode("This is a **BOLD** statement!", "text")
-        want = [TextNode("This is a ", "text"), TextNode("BOLD", "bold"), TextNode(" statement!", "text")]
-        got = split_nodes_delimiter([bold], '**', "bold")
+        want = [
+            TextNode("This is a ", "text"),
+            TextNode("BOLD", "bold"),
+            TextNode(" statement!", "text"),
+        ]
+        got = split_nodes_delimiter([bold], "**", "bold")
         self.assertEqual(want, got)
 
     def test_multi_nodes(self):
         node = TextNode("This is text with a `code block` word", "code")
         bold = TextNode("This is a *BOLD* statement!", "bold")
-        want = [TextNode("This is text with a `code block` word", "text"), TextNode("This is a ", "text"), TextNode("BOLD", "bold"), TextNode(" statement!", "text")]
+        want = [
+            TextNode("This is text with a `code block` word", "text"),
+            TextNode("This is a ", "text"),
+            TextNode("BOLD", "bold"),
+            TextNode(" statement!", "text"),
+        ]
         got = split_nodes_delimiter([node, bold], "*", "bold")
+        self.assertEqual(want, got)
+
+
+class TestSplitImageNodes(unittest.TestCase):
+    def test_parse_links_and_images(self):
+        text = "This is text with an [image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)"
+        got = parse_links_and_images(text)
+        want = ["This is text with an ", "[image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png)", " and another ", "![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)"]
+        self.assertEqual(want, got)
+
+    def test_parse_links_and_images_just_images(self):
+        text = "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)"
+        got = parse_links_and_images(text)
+        want = ["This is text with an ", "![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png)", " and another ", "![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)"]
+        self.assertEqual(want, got)
+
+    def test_parse_links_and_images_just_links(self):
+        text = "This is text with an [image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another [second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)"
+        got = parse_links_and_images(text)
+        want = ["This is text with an ", "[image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png)", " and another ", "[second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)"]
+        self.assertEqual(want, got)
+
+    def test_scan_until_not_text(self):
+        text = "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)"
+        got = scan_until_not_text(text)
+        want = ("This is text with an ", 21)
+        # want = ("This is text with an ", "![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)")
+        self.assertEqual(want, got)
+
+    def test_scan_until_not_text_bad_unclosed_square(self):
+        text = "This is text with an ![image(https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)"
+        got = scan_until_not_text(text)
+        want = (
+            "This is text with an ![image(https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)",
+            -1,
+        )
+        self.assertEqual(want, got)
+
+    def test_scan_until_not_text_bad_unclosed_round(self):
+        text = "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)"
+        got = scan_until_not_text(text)
+        want = (
+            "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)",
+            -1,
+        )
+        self.assertEqual(want, got)
+
+    def test_scan_until_not_text_bad_unopened_round(self):
+        text = "This is text with an ![image]https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)"
+        got = scan_until_not_text(text)
+        want = (
+            "This is text with an ![image]https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)",
+            -1,
+        )
+        self.assertEqual(want, got)
+
+    def test_split_nodes_image(self):
+        node = TextNode(
+            "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)",
+            "image",
+        )
+
+        got = split_nodes_image([node])
+        want = [
+            TextNode("This is text with an ", "text"),
+            TextNode(
+                "image",
+                "image",
+                "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png",
+            ),
+            TextNode(" and another ", "text"),
+            TextNode(
+                "second image",
+                "image",
+                "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png",
+            ),
+        ]
         self.assertEqual(want, got)
