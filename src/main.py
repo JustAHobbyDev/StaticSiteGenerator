9ca495236af4175ac50f03db3e35e2f5b051e259
Daniel Schmidt <justahobbydev@proton.me>
2024-09-22 00:17:48 -0500

    Ch 5: Website - 1: Copy Static (https://www.boot.dev/lessons/abd575ab-62df-4935-8746-fa440c4925d2)

--- src/main.py
+++ src/main.py
@@ -1,88 +1,30 @@
-from textnode import TextNode
-from leafnode import LeafNode
+from pathlib import Path
+import shutil
+
+def build(source, target):
+    root = Path.cwd()
+    src_dir = root / source
+    target = root / target
+    shutil.rmtree(target)
+    target.mkdir()
+    recursive_copy(src_dir, target)
 
 
-def text_node_to_html_node(text_node):
-    text_types = {
-        "text": {"tag": None, "content": text_node.text, "props": {}},
-        "bold": {"tag": "b", "content": text_node.text, "props": {}},
-        "italic": {"tag": "i", "content": text_node.text, "props": {}},
-        "code": {"tag": "code", "content": text_node.text, "props": {}},
-        "link": {"tag": "a", "content": text_node.text, "props": {}},
-        "image": {
-            "tag": "img",
-            "content": "",
-            "props": {"alt": text_node.text, "src": text_node.url},
-        },
-    }
+def recursive_copy(source, target):
 
-    text_type = text_types[text_node.text_type]
-    tag = text_type["tag"]
-    content = text_type["content"]
-    props = text_type["props"]
-
-    leaf_node = LeafNode(tag, content, props)
-    return leaf_node
+    for child in source.iterdir():
+        target_child = target / child.name
+        if child.is_dir():
+            print(f'Copying directory {child} to {target / child.name}')
+            target_child.mkdir()
+            print(f'recursive_copy({child}, {target / child.name})')
+            print("---\n")
+            recursive_copy(child, target_child)
+        if child.is_file():
+            print(f'Copying file {child.name} to {target_child}')
+            shutil.copy(child, target)
+            print("---\n")
 
 
-def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    final_nodes = []
-    for node in old_nodes:
-        if node.__class__.__name__ == "TextNode":
-            future_textnodes = []
-            delimited = False
-            i, j = 0, 0
-            while j < len(node.text):
-                character = node.text[j]
-                # print(character)
-                if character is delimiter:
-                    content = node.text[i:j]
-                    future_textnodes.append({"content": content, "delimited": delimited})
-                    delimited = not delimited
-                    i = j + 1
-                    j = i + 1
-                else:
-                    if j < len(node.text):
-                        j += 1
-            else:
-                # delimited should be False after iterating through the text
-                if delimited:
-                    raise SyntaxError("Invalid Markdown: Unclosed delimiter found.") 
-                print(node.text[i:j])
-                content = node.text[i:j]
-                future_textnodes.append({"content": content, "delimited": delimited})
-
-                for future_textnode in future_textnodes:
-                    if not future_textnode["delimited"]:
-                        future_textnode_type = "text"
-                    else:
-                        future_textnode_type = text_type
-                    final_nodes.append(TextNode(future_textnode["content"], future_textnode_type, node.url))
-
-        else:
-            final_nodes.append(node)
-
-    return final_nodes
-
-
-# tn = TextNode("Some random text", "italic", "https://boot.dev")
-
-# print(tn)
-
-# print(text_node_to_html_node(tn))
-
-# img_tn = TextNode("A pretty picture", "image", "http://unsplash.com/photos/a-bird-is-standing-on-a-hill-at-sunset-5AiN3pLyJLU")
-# img = text_node_to_html_node(img_tn)
-# print(img.to_html())
-
-node = TextNode("This is text with a `code block` word", "text")
-print(f"type of node: {type(node)}")
-new_nodes = split_nodes_delimiter([node], "`", "code")
-print(new_nodes)
-
-
-bad_node = TextNode("This is text with a `code block word", "text")
-try:
-    new_nodes = split_nodes_delimiter([node, bad_node], "`", "code")
-except Exception as e:
-    print(f"Error: {e}")
+if __name__ == '__main__':
+    build('static', 'public')
